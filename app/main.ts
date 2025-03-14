e1f7bf46f6873e60521f162711483b2baba95f04
yjgaia <yjdev88@gmail.com>
2025-03-14 19:29:53 +0900

    Add SVG icons dependency and integrate icons into AppCompConfig; enhance image viewer styles

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,14 @@
 import { Router } from "@common-module/app";
+import { AppCompConfig } from "@common-module/app-components";
+import { CloseIcon, NextIcon, PrevIcon } from "@common-module/svg-icons";
 import ConfirmView from "./views/ConfirmView.js";
 import ImageViewerView from "./views/ImageViewerView.js";
 import IndexView from "./views/IndexView.js";
 import TabView from "./views/TabView.js";
+
+AppCompConfig.CloseIcon = CloseIcon;
+AppCompConfig.PrevIcon = PrevIcon;
+AppCompConfig.NextIcon = NextIcon;
 
 Router
   .add("/", IndexView)
